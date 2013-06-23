2c2e1f5719646c9448dc716730fce030cb61706e
Mansoor Masoudifard <mansoor.masoudifard@gmail.com>
2013-06-23 12:33:46 +0430

    version 0.8 fix resize bug better coding

--- js.js
+++ js.js
@@ -6,13 +6,23 @@
     $dokme = $('.dokme'),
     xpos,
     btnWidth = 70,
-    btnWidthTtow = 70/2,
-    $btnWidthArray= [btnWidthTtow,btnWidthTtow+(btnWidth*1),btnWidthTtow+(btnWidth*2),btnWidthTtow+(btnWidth*3),btnWidthTtow+(btnWidth*4),btnWidthTtow+(btnWidth*5),btnWidthTtow+(btnWidth*6),btnWidthTtow+(btnWidth*7),btnWidthTtow+(btnWidth*8)],
+    btnWidthTtow = btnWidth/2,
+    btnWidthArray = function(i){
+      return btnWidthTtow+(btnWidth*i);
+    },
     $slideshow = $('.slideshow'),
     $train = $slideshow.find('.train'),
     $slidesWidth = $train.find('div').width(),
     $lis = $('.bar li'),
     activeTemp = $lis.find('.active'),
+    resize = function(i){
+      $(window).resize(function(){
+        $slidesWidth = $train.find('div').width();
+        $train.css({
+          'left':$slidesWidth*(-i)
+        });
+      });
+    },
     moveDokme = function(){
       $dokme.draggable({
         containment: $btn,
@@ -23,19 +33,18 @@
         stop: function(event, ui) {
           xpos = ui.position.left;
           for(var i=0;i<9;i++){
-            if(xpos<$btnWidthArray[i]){
-              var
-                n=i;
-                i=9;
+            btnWidthArray(i);
+            if(xpos<btnWidthArray(i)){
               $dokme.animate({
-                'left':btnWidth*n
+                'left':btnWidth*i
               },200);
               $train.animate({
-                'left': $slidesWidth*(-n)
+                'left': $slidesWidth*(-i)
               },1000);
               activeTemp.removeClass();
-              $lis.eq(n).addClass('active');
-              activeTemp = $lis.eq(n);
+              $lis.eq(i).addClass('active');
+              activeTemp = $lis.eq(i);
+              break;
             }
           }
         }
@@ -52,6 +61,7 @@
         activeTemp.removeClass();
         $(this).addClass('active');
         activeTemp = $(this);
+        resize(i);
       });
     },
     lisClick = function(){
@@ -64,10 +74,4 @@
       lisClick();
     };
     allFunction();
-    $(window).resize(function(){
-      $slidesWidth = $train.find('div').width();
-      $train.css({
-        'left':$slidesWidth*(-1)
-      });
-    });
 })(window.jQuery);
